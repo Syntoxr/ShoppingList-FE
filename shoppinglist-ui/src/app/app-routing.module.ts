757962753f39972e1263a49e6b9266de2c9631e1
Syntox <syntox@another-server.de>
2022-04-04 22:11:20 +0000

    added environment specifiv paths

--- shoppinglist-ui/src/app/app-routing.module.ts
+++ shoppinglist-ui/src/app/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/shopping-list', pathMatch: 'full' },
-  { path: 'shopping-list', component: ShoppingListComponent },
-
-  { path: '**', redirectTo: '/shopping-list' },
-];
+const routes: Routes = [...getEnvironmentPaths()];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
+
+function getEnvironmentPaths() {
+  if (environment.mock) {
+    return [
+      { path: '', component: ShoppingListComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: '' },
+    ];
+  } else {
+    return [
+      { path: '', redirectTo: '/shopping-list', pathMatch: 'full' },
+      { path: 'shopping-list', component: ShoppingListComponent },
+      { path: '**', redirectTo: '/shopping-list' },
+    ];
+  }
+}
