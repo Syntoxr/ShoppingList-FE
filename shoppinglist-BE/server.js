effd251a7abf758b06294b690ce9e621c2ecca38
Syntox <syntox@another-server.de>
2022-04-09 01:23:59 +0000

    created node project

---
+++ shoppinglist-BE/server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+
+
+
+app.listen(8000, () => {
+  console.log('Server started!')
+})
+
+
+
